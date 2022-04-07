6736fe5dc2321bacddaa7f02a14bdf134ddeeead
Rekha <qa.rekhapatil@gmail.com>
2022-04-07 11:18:03 -0700

    added author name

--- src/KiteAppPOMClasses/KiteHomePage.java
+++ src/KiteAppPOMClasses/KiteHomePage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+// Author Rekha Patil
 public class KiteHomePage {
 	@FindBy(xpath="//span[@class='user-id']")private WebElement userID;
 	
